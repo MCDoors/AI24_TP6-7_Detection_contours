7eb851fbdc5282dbe4ffa2a4c7e7218d608e7b9b
MCDoors <40465432+MCDoors@users.noreply.github.com>
2019-01-12 01:42:51 +0100

    1.0.0

--- image.asm
+++ image.asm
@@ -1,6 +1,9 @@
+; Nicolas HUET
+; Guillaume NIBERT
+
 ; image.asm
 ;
-; MI01 - TP Assembleur 2 à 5
+; AI24 - TP Assembleur 2 à 5
 ;
 ; Réalise le traitement d'une image bitmap 32 bits par pixel.
 
@@ -35,24 +38,20 @@ process_image_asm 	proc		; Point d'entrée de la fonction
 		push	rbp
 		push	rdi
 		push	rsi
-		push	rsp
 		push	r12
 		push	r13
 		push	r14
 		push	r15
 
-		; Le passage de paramètre suis la convention fastcall 64
+		; Le passage de paramètre suis la convension fastcall x64
 		; Les 4 premiers arguments sont passés dans rcx, rdx, r8 et r9 et le reste est mis sur la pile
 		; biWidth	-> rcx
 		; biHeight	-> rdx
 		; img_src	-> r8
 		; img_temp1	-> r9
-		; Les 4 premiers sont dans le shadow space
-		
-		; ********* IL FAUT LES METTRES, POURQUOI CA MARCHE ? SUREMENT HISTOIRE DU CALL
-		
-		; img_temp2	-> [rsp + 48]
-		; img_dest	-> [rsp + 56]
+				
+		; img_temp2	-> [rsp + 40]
+		; img_dest	-> [rsp + 48]
 		
 		mov r12, rcx	; On sauvegarde le premier paramètre dans le registre r12 car rdx est modifié après.
 		mov r15, rdx	; biHeight nombre de lignes
@@ -63,50 +62,39 @@ process_image_asm 	proc		; Point d'entrée de la fonction
 		
 ; Boucle en partant du dernier pixel jusqu'au premier pixel.
 color:
-		cmp rcx, 0
-		je init_registres 						; Dès qu'on a effectué le niveau de gris (donc traité le premier pixel), on passe au TP6
 		
 		movzx rax, byte ptr [r8 + rcx*4]		; BLEU
-		shl rax, 8								; On déplace le byte ptr de 8 bits vers la gauche (et donc ce qui avait à la place du byte ptr devient des 0) Tout ce qui est à gauche disparaît.
 		imul rax, 1Dh							; Réalise la multiplication suivante : 1Dh * "Bleu"
 		mov r13, rax							; r13 stocke le résultat du niveau de gris
 		
 		movzx rax, byte ptr [r8 + rcx*4 + 1]	; VERT (même démarche)
-		shl rax, 8
 		imul rax, 96h
 		add r13, rax
 		
 		movzx rax, byte ptr [r8 + rcx*4 + 2]	; ROUGE (même démarche)
-		shl rax, 8
 		imul rax, 4Ch
 		add r13, rax
 		
 		; Après ces instructions : r13 = 4C * Rouge + 96 * Vert + 1D * Bleu
 		
-		shr r13, 16								; On déplace le r13 de 16 bits vers la droite (on supprime donc la moitié), pour la virgule
+		shr r13, 8								; On déplace le r13 de 8 bits vers la droite (on supprime donc la moitié), pour la virgule
 		
 		mov byte ptr [r9 + rcx*4], r13b			; On stocke dans Bleu et la technique du niveau gris, pour les autres composantes on met 0 comme indiqué dans le TP
 		mov byte ptr [r9 + rcx*4 + 1], 0
 		mov byte ptr [r9 + rcx*4 + 2], 0
 		mov byte ptr [r9 + rcx*4 + 3], 0
 		
-		dec rcx				; Décrémentation de l'index du pixel (cf, boucle en partant du dernier pixel, on remonte donc)
-		cmp rcx, 0			; Tant qu'on n'a pas traité le premier pixel
-		jne color			; On continue dans la boucle...
+		dec rcx	; Décrémentation de l'index du pixel pour passer au traitement du pixel suivant (le précédent puisqu'on parcourt l'image de la fin vers le début)
+		cmp rcx, 0	; Tant que rcx n'est pas égal à 0...
+		jne color	; ...On continue à réaliser la technique du niveau du gris sur les pixels restants à traiter.
 
 		;***************************************
 		;				TP6/7				   *
 		;***************************************
-		
-		; Q3.2
-
-init_registres:				; Q3.2
 
 		mov r8, r9			; On stocke dans le registre r8, l'adresse du premier pixel auquel appliquer le masque (Source)
-		
-		mov r9, r11			; On stocke dans le registre r9, l'adresse du pixel de img_temp2 dans lequel on veut stocker le résultat. ATTENTION les contours ne sont pas comptés (32 de shadow space + 8 pour être au dessus de img_temp2
-
-		mov r10, r12		; On stocke dans r10, la taille d'une ligne en pixel (cf ligne 55) (la taille d'une ligne, c'est le nombre de colonnes)..
+		mov r9, r11			; On stocke dans le registre r9, l'adresse du pixel de img_temp2 dans lequel on veut stocker le résultat. ATTENTION les contours ne sont pas comptés !
+		mov r10, r12		; On stocke dans r10, la taille d'une ligne en pixel (cf ligne 54) (la taille d'une ligne, c'est le nombre de colonnes)..
 		
 		
 		; rax = 4*biWidth + 4
@@ -120,36 +108,32 @@ init_registres:				; Q3.2
 		sub rdx, 2				; On enlève les 2 dernières lignes
 		
 		; Q3.3
+		
 		; rdx : nombre de lignes initialisé à la nbtotallignes-2
 		; rcx : nombre de colonnes initialisé à nbtotalcolonnes-2
-		; puisque les contours sont retirés en raison de la matrice de convolution de Sobel
+		; Puisque les contours sont retirés en raison de la matrice de convolution de Sobel
 		
-		; INITIALISATION DE rdx ET rcx
-				
 		; Q3.3.1
 		
-		
-		;mov rdx, rcx	; rdx = biWidth (le nombre de lignes)
-		;sub rdx, 2		; rdx = biWidth - 2 (le nombre de lignes - 2) ; Initialisation de rdx au début du traitement de l'image
-		
 		; Quelle valeur affecter à r8 à la fin du traitement d'une ligne pour passer à la ligne suivante ?
-		; r8 - 2 dernières colonnes
-		
+		; A la fin du traitement d’une ligne, pour passer à la ligne suivante, on ajoute 8 octets à r8 car on saute 2 pixels (rappel : 1 pixel correspond à 4 octets).
 		
 		; Quelle valeur affecter à r9 à la fin du traitement d'une ligne pour passer à la ligne suivante ?
-		; r9 - 1 dernière colonne
+		; A la fin du traitement d’une ligne, pour passer à la ligne suivante, on ajoute 8 octets à r9 car on saute 2 pixels (rappel : 1 pixel correspond à 4 octets).
+		
+		; Q3.3.2
+		
+		; r8 et r9 sont incrémentés de 4 pour passer au pixel suivant.
 		
 lignes:
 		
 		cmp rdx, 0			; S'il ne reste plus de ligne...
 		je fin_pour_lignes	; ...alors, on termine l'agorithme.
-		mov rcx, r12		; Récupération du nombre de colonnes (cf ligne 55)
+		mov rcx, r12		; Récupération du nombre de colonnes (cf ligne 54)
 		sub rcx, 2			; On enlève les deux derniers pixels à chaque fois.
 		
-		;add	r8, 1		; Passer au pixel suivant
-		
 		; Des que r8 < col - 2
-		; On passe à la ligne suivante
+		; On passe à la ligne suivante ("r8 = r8 + 8")
 
 colonnes:
 		
@@ -187,9 +171,7 @@ colonnes:
 		cmp r15, 0 	; Puis vérifier si c'est positif
 		jge calcul_Gy ; si c'est positif ou égal à 0, alors on saute à calcul_Gy pour changer le signe sinon, on passe le code 
 
-; valeur_absolue_Gx
-
-		neg r15
+		neg r15 ; Gestion de la valeur absolue de Gx
 
 calcul_Gy:
 
@@ -228,9 +210,7 @@ calcul_Gy:
 		cmp r15, 0 ; Idem que pour |Gx|
 		jge inversion_intensite
 
-; valeur_absolue_Gy
-
-		neg r15
+		neg r15	; Gestion de la valeur absolue de Gy
 
 inversion_intensite:    	; cf Algo p2/4 du TP (ligne : G <- 255 - G)
 
@@ -240,8 +220,6 @@ inversion_intensite:    	; cf Algo p2/4 du TP (ligne : G <- 255 - G)
         
         cmp r14, 0			; Si G >= 0......
         jge image_resultante; ......On stocke dans le pixel de destination
-
-; G_negatif
 
         mov r14, 0 ; ......Sinon G < 0 alors G = 0 puis on stocke dans le pixel de destination.
 
@@ -272,7 +250,6 @@ fin_pour_lignes:
 		pop		r14
 		pop		r13
 		pop		r12
-		pop		rsp
 		pop		rsi
 		pop		rdi
 		pop		rbp
